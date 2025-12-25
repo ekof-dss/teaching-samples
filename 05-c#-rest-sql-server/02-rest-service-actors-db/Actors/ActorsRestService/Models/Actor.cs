0c745ff513c856140db415d97ce59ad61575627a
Vladimir Filipovic <vladofilipovic@hotmail.com>
2025-12-25 21:56:33 +0100

    reorganization

--- 05-c#-rest-sql-server/02-rest-service-actors-db/Actors/ActorsRestService/Models/Actor.cs
+++ 05-c#-rest-sql-server/02-rest-service-actors-db/Actors/ActorsRestService/Models/Actor.cs
@@ -1,14 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ActorsRestService.Models
 {
-    public partial class Actor
+
+    [Table("actor")]
+    public class Actor
     {
-        public long Id { get; set; }
+        [Key]
+        [Column("actor_id", TypeName = "bigint")]
+        public long ActorId { get; set; }
+
+        [Required]
+        [Column("last_name", TypeName = "nvarchar(50)")]
         public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string CountryCode { get; set; }
-        public DateTime? DateOfBith { get; set; }
+
+        [Required]
+        [Column("first_name", TypeName = "nvarchar(50)")]
+        public string? FirstName { get; set; }
+
+        [Column("country_id", TypeName = "bigint")]
+        public long? CountryId { get; set; }
+
+        [Column("date_of_birth", TypeName = "date")]
+        public DateTime? DateOfBirth { get; set; }
+
+        public virtual Country? Country { get; set; }
+
     }
+
 }
---
+++ 05-c#-rest-sql-server/02-rest-service-actors-db/Actors/ActorsRestService/Models/Country.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsRestService.Models
+{
+    [Table("country")]
+    public class Country
+    {
+        [Key]
+        [Column("country_id", TypeName="bigint")]
+        public long CountryId { get; set; }
+
+        [Required]
+        [Column("country_name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("country_code")]
+        public string Code { get; set; }
+
+        public virtual ICollection<Actor> Actors { get; set; }
+    }
+}
